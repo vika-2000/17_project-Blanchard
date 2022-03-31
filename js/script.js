c7e4f93e3826bbd2ab8b67b7d2866c16e49bf560
vika-2000 <61766232+vika-2000@users.noreply.github.com>
2022-03-31 20:20:43 +1000

    моя работа

--- js/script.js
+++ js/script.js
@@ -8,3 +8,72 @@ let heroSlider = new Swiper('.art__swiper' , {
   },
   effect: 'fade',
 });
+
+// dropdown
+$('.dropdown__btn').click(function () {
+  $(this).toggleClass('open');
+  $('.dropdown__btn').not(this).removeClass('open');
+});
+
+// gallery слайдер
+
+const gallerySlider = new Swiper('.gallery__swiper-container', {
+  pagination: {
+    el: '.gallery__pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.gallery__btn-next',
+    prevEl: '.gallery__btn-prev',
+  },
+  a11y: {
+    prevSlideMessage: 'Предыдущие слайды',
+    nextSlideMessage: 'Следующие слайды',
+  },
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  slidesPerGroup: 3,
+  spaceBetween: 50,
+  speed: 800,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      grid: {
+        rows: 1,
+      },
+      spaceBetween: 15,
+    },
+    577: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      grid: {
+        rows: 2,
+      },
+      spaceBetween: 34,
+    },
+    1400: {
+      loop: false,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      grid: {
+        rows: 2,
+      },
+      spaceBetween: 50,
+      speed: 600,
+    },
+  },
+});
+
+
+// gallery select
+const galleryChoices = new Choices('.gallery__select', {
+  searchEnabled: false, //надо
+  itemSelectText: null, //надо
+  searchChoices: false,
+  classNames: {
+    containerOuter: 'choices gallery__choices',
+  },
+});
